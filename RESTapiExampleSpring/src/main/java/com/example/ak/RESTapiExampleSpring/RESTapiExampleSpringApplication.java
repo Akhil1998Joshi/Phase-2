edf0a338c0e857d7bddc126b41ddef7b1075a378
Akhil Joshi <akhil.joshi1997@gmail.com>
2019-04-08 02:31:31 +0530

    Add files via upload

---
+++ RESTapiExampleSpring/src/main/java/com/example/ak/RESTapiExampleSpring/RESTapiExampleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ak.RESTapiExampleSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages={"com.example.ak.RESTapiExampleSpring"})
+public class RESTapiExampleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RESTapiExampleSpringApplication.class, args);
+	}
+
+}
